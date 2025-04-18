f43f1347c3e132c8c79c1ae4dd6bfb741eea975f
sakshi16-b <sakshibisht016@gmail.com>
2025-04-18 08:21:15 +0000

    dark mode codebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,43 @@
+import { useState } from "react";
 import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  const [darkMode, setDarkMode] = useState("dark");
+
+  useEffect(() => {
+    console.log(darkMode);
+    if (darkMode == "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [darkMode]);
   return (
     <>
-      <div className="grid grid-cols-5 h-screen text-center">
-        <div className="col-span-1 bg-zinc-800 text-2xl">Heeloo</div>
-        <div className="col-span-4 p-10">
-          <div className="container h-160"></div>
+      <div className={darkMode == "dark" ? "dark" : "light"}>
+        <div className="grid grid-cols-5 h-screen text-center">
+          <select
+            onChange={(e) => setDarkMode(e.target.value)}
+            className="fixed text-white bottom-0 p-5"
+          >
+            <option value="dark" className="bg-gray-50 text-black">
+              Dark
+            </option>
+            <option value="light">Light</option>
+          </select>
+          <div className="col-span-1 bg-zinc-800 text-2xl">Heeloo</div>
+          <div className="col-span-4 p-10">
+            <div className="container h-160"></div>
 
-          <div className="bg-zinc-800  p-1 w-1/2 text-white border-zinc-400 m-auto rounded-2xl flex">
-            <input
-              type="text"
-              className="w-full h-full p-3 outline-none"
-              placeholder="Ask me Anything"
-            />
-            <button>Ask</button>
+            <div className="bg-zinc-800  p-1 w-1/2 text-white border-zinc-400 m-auto rounded-2xl flex">
+              <input
+                type="text"
+                className="w-full h-full p-3 outline-none"
+                placeholder="Ask me Anything"
+              />
+              <button>Ask</button>
+            </div>
           </div>
         </div>
       </div>
