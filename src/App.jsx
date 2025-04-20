2cb860051f37fb346034f3274766ad1b44f8eca1
sakshi16-b <sakshibisht016@gmail.com>
2025-04-20 12:08:22 +0000

    implement of gemini key

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,60 @@
 import { useState } from "react";
 import "./App.css";
 import { useEffect } from "react";
+import { URL } from "./constants ";
 
 function App() {
-  const [darkMode, setDarkMode] = useState("dark");
+  const [question, setQuestion] = useState("");
+  const payload = {
+    contents: [
+      {
+        parts: [{ text: "Explain how AI works" }],
+      },
+    ],
+  };
 
-  useEffect(() => {
-    console.log(darkMode);
-    if (darkMode == "dark") {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-  }, [darkMode]);
+  const askQuestion = async () => {
+    let response = await fetch(URL, {
+      method: "POST",
+      body: JSON.stringify(payload),
+    });
+    response = await response.json();
+    console.log(response.candidates[0].content.parts[0].text);
+  };
+
+  // useEffect(() => {
+  //   console.log(darkMode);
+  //   if (darkMode == "dark") {
+  //     document.documentElement.classList.add("dark");
+  //   } else {
+  //     document.documentElement.classList.remove("dark");
+  //   }
+  // }, [darkMode]);
   return (
     <>
-      <div className={darkMode == "dark" ? "dark" : "light"}>
-        <div className="grid grid-cols-5 h-screen text-center">
-          <select
-            onChange={(e) => setDarkMode(e.target.value)}
-            className="fixed text-white bottom-0 p-5"
-          >
-            <option value="dark" className="bg-gray-50 text-black">
-              Dark
-            </option>
-            <option value="light">Light</option>
-          </select>
-          <div className="col-span-1 bg-zinc-800 text-2xl">Heeloo</div>
-          <div className="col-span-4 p-10">
-            <div className="container h-160"></div>
+      <div className="grid grid-cols-5 h-screen text-center">
+        <select
+          // onChange={(e) => setDarkMode(e.target.value)}
+          className="fixed text-white bottom-0 p-5"
+        >
+          <option value="dark" className="bg-gray-50 text-black">
+            Dark
+          </option>
+          <option value="light">Light</option>
+        </select>
+        <div className="col-span-1 bg-zinc-800 text-2xl">Heeloo</div>
+        <div className="col-span-4 p-10">
+          <div className="container h-160"></div>
 
-            <div className="bg-zinc-800  p-1 w-1/2 text-white border-zinc-400 m-auto rounded-2xl flex">
-              <input
-                type="text"
-                className="w-full h-full p-3 outline-none"
-                placeholder="Ask me Anything"
-              />
-              <button>Ask</button>
-            </div>
+          <div className="bg-zinc-800  p-1 pr-5 w-1/2 text-white border-zinc-700 m-auto rounded-4xl flex h-16">
+            <input
+              type="text"
+              className="w-full h-full p-3 outline-none"
+              value={question}
+              onChange={(e) => setQuestion(e.target.value)}
+              placeholder="Ask me Anything"
+            />
+            <button onClick={askQuestion}> Ask</button>
           </div>
         </div>
       </div>
