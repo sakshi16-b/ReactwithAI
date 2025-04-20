ccb02adad5f33b01c5e81d83a5aed97769df7556
sakshi16-b <sakshibisht016@gmail.com>
2025-04-20 12:53:50 +0000

    chat functionality complete

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import { URL } from "./constants ";
 
 function App() {
   const [question, setQuestion] = useState("");
+  const [result, setResult] = useState(undefined);
   const payload = {
     contents: [
       {
-        parts: [{ text: "Explain how AI works" }],
+        parts: [{ text: question }],
       },
     ],
   };
@@ -20,6 +21,7 @@ function App() {
     });
     response = await response.json();
     console.log(response.candidates[0].content.parts[0].text);
+    setResult(response.candidates[0].content.parts[0].text);
   };
 
   // useEffect(() => {
@@ -44,8 +46,9 @@ function App() {
         </select>
         <div className="col-span-1 bg-zinc-800 text-2xl">Heeloo</div>
         <div className="col-span-4 p-10">
-          <div className="container h-160"></div>
-
+          <div className="container h-160">
+            <div className="text-white">{result}</div>
+          </div>
           <div className="bg-zinc-800  p-1 pr-5 w-1/2 text-white border-zinc-700 m-auto rounded-4xl flex h-16">
             <input
               type="text"
