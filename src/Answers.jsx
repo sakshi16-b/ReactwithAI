4711be9abd1237650512186faaa1e5056ae5a379
sakshi16-b <sakshibisht016@gmail.com>
2025-04-21 08:19:49 +0000

    api displayed removed commas using regex

--- src/Answers.jsx
+++ src/Answers.jsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from "react";
+import { checkHeading, replaceHeadingStarts } from "./helper";
+
 const Answers = ({ answer, key }) => {
-  console.log(answer, key);
+  const [heading, setHeading] = useState(false);
+  const [answers, setAnswers] = useState(answer);
+  console.log(answer, checkHeading(answer));
+
+  useEffect(() => {
+    if (checkHeading(answer)) {
+      console.log(answer);
+      setHeading(true);
+      setAnswers(replaceHeadingStarts(answer));
+    }
+  }, []);
   return (
     <>
-      <p>{answer}</p>
+      {heading ? (
+        <span className="pt-2 text-lg block">{answer}</span>
+      ) : (
+        <span className="text-sm">{answer}</span>
+      )}
     </>
   );
 };
